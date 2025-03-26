520c9ba7f3a3f3210d3aa05d1491fa7e3e08691d
ArtificialBakingTrays <150090215+ArtificialBakingTrays@users.noreply.github.com>
2025-03-26 12:26:52 +0000

    Artificial Weaponry V4
    Parasitical Implant is now fully functional
    Needs to have some vfx but its a working thing now.

--- lua/weapons/armparasite.lua
+++ lua/weapons/armparasite.lua
@@ -6,7 +6,7 @@ SWEP.IconOverride = "vgui/weaponvgui/placehold_generi.png"
 
 SWEP.Spawnable = true
 SWEP.AdminOnly = false
-SWEP.DrawCrosshair = false
+SWEP.DrawCrosshair = true
 SWEP.ViewModel	= "models/weapons/c_crossbow.mdl"
 SWEP.WorldModel	= "models/weapons/w_crossbow.mdl"
 SWEP.DrawAmmo = true
@@ -30,6 +30,7 @@ SWEP.Secondary.Ammo		= "none"
 function SWEP:PrimaryAttack()
 	if self:Clip1() <= 0 then return end
 	if self:Clip1() <= 30 then self.Dmg = 16 end
+
 	self:SendWeaponAnim( ACT_VM_PRIMARYATTACK )
 	self:TakePrimaryAmmo( 1 )
 
@@ -37,9 +38,38 @@ function SWEP:PrimaryAttack()
 
 	self:EmitSound( "tray_sounds/parasite_energy.mp3", 55, math.random(130, 140), 1, 1 )
 	self:EmitSound( "weapons/ar2/ar2_altfire.wav", 90, math.random(130, 135), 1, 6 )
-end
 
-function SWEP:SecondaryAttack() end --will be used for scoping like Slabshot
+	local owner = self:GetOwner()
+	owner:LagCompensation( true )
+
+	owner:FireBullets {
+		Src = owner:GetShootPos(),
+		Dir = owner:GetAimVector(),
+		Damage = 55,
+		Attacker = owner,
+		Callback = function( attacker, tr, dmg )
+			if tr.Entity:IsValid() and ( tr.Entity:IsPlayer() or tr.Entity:IsNPC() ) then
+				if SERVER then
+					local newarmor = owner:Armor() + math.floor(dmg:GetDamage() / 4)
+					if newarmor > 100 then newarmor = 100 end
+
+					util.BlastDamage( dmg:GetInflictor(), owner, tr.HitPos, 95, dmg:GetDamage() / 4)
+
+					owner:SetArmor( newarmor )
+
+					tr.Entity:EmitSound( "tray_sounds/parsite_multihit.mp3", 100, math.random(100, 115), 1, 6 )
+				end
+
+				local effectdata = EffectData()
+				effectdata:SetOrigin( tr.HitPos )
+
+				util.Effect("HelicopterMegaBomb", effectdata, true)
+			end
+		end,
+	}
+
+	owner:LagCompensation( false )
+end
 
 function SWEP:CustomAmmoDisplay()
 	self.AmmoDisplay = self.AmmoDisplay or {}
@@ -90,4 +120,21 @@ function SWEP:Think()
 
 	self:EmitSound( "weapons/bugbait/bugbait_squeeze3.wav", 75, math.random(140, 150), 1, 6 )
 	self:EmitSound( "weapons/ar2/ar2_reload_rotate.wav", 75, 100, 1, 6 )
+end
+
+function SWEP:SetScoped( bool ) self:SetDTBool( 0, bool ) end
+function SWEP:GetScoped() return self:GetDTBool( 0 ) end
+function SWEP:SecondaryAttack() self:SetScoped( not self:GetScoped() ) end
+
+function SWEP:TranslateFOV( fov )
+	self.LastFOV = fov
+	if self:GetScoped() then
+		return 25
+	end
+end
+
+function SWEP:AdjustMouseSensitivity()
+	if self:GetScoped() then
+		return 25 / self.LastFOV
+	end
 end
