ed89452e8235c6f7fed292d967694f91a4de8ec2
ArtificialBakingTrays <150090215+ArtificialBakingTrays@users.noreply.github.com>
2025-05-28 15:52:17 +0100

    V12 Making Undeclared Work
    Added new sound for Undeclared charging
    Made Undeclared function properly now

--- lua/weapons/undeclared.lua
+++ lua/weapons/undeclared.lua
@@ -20,7 +20,108 @@ SWEP.Primary.DefaultClip = 0
 SWEP.Primary.Automatic	= true
 SWEP.Primary.Ammo = "Battery"
 
-SWEP.Secondary.ClipSize		= -1
-SWEP.Secondary.DefaultClip	= -1
-SWEP.Secondary.Automatic	= false
-SWEP.Secondary.Ammo		= "none"
+SWEP.Secondary.ClipSize		= 0
+SWEP.Secondary.DefaultClip	= 0
+SWEP.Secondary.Automatic	= true
+SWEP.Secondary.Ammo		= "Battery"
+
+local bannedLUT = {
+	["CHudAmmo"]      = true,
+}
+
+function SWEP:HUDShouldDraw(element)
+	if bannedLUT[element] then
+		return false
+	end
+	return true
+end
+
+function SWEP:PrimaryAttack()
+	local owner = self:GetOwner()
+	if self:Clip2() == 0 then return end
+	self:SetNextPrimaryFire( CurTime() + 1 )
+
+	local ExplDMG = self:Clip2() * 1.25
+	local ExplRad = 60 + (self:Clip2() * 2)
+
+	local ExplPos = owner:GetShootPos() + ( owner:GetAimVector() * 10 ),
+
+	owner:LagCompensation( true )
+
+		util.BlastDamage( self, owner, ExplPos, ExplRad, ExplDMG )
+		util.ScreenShake( ExplPos, ExplDMG, 40, 1, ExplRad, true )
+
+		self:EmitSound( "weapons/mortar/mortar_explode2.wav", 100, math.random(80, 90) - (self:Clip2() * 0.1), 1, 6 )
+		self:EmitSound( "tray_sounds/parsite_multihit.mp3", 100, math.random(80, 90) - (self:Clip2() * 0.1), 0.3, 6 )
+
+	owner:LagCompensation( false )
+
+	self:SetClip1( 0 )
+	self:SetClip2( 0 )
+end
+
+
+function SWEP:SecondaryAttack()
+	self:SetNextSecondaryFire( CurTime() + 0.7 )
+	if self:Clip1() == 0 then self:SetClip1( 1 ) end
+
+	self:SetClip2( self:Clip2() + 50 )
+
+	local MaxPit = math.random(100 + self:Clip2(), 105 + self:Clip2())
+
+	self:EmitSound( "artiwepsv3/ding.mp3", 100, math.Clamp(MaxPit, 100, 170), 1.5, 6 )
+
+end
+
+function SWEP:SpecialParticles()
+local tr =  LocalPlayer():GetEyeTrace()
+local pos = tr.HitPos + tr.HitNormal * 100 -- The origin position of the effect
+
+local emitter = ParticleEmitter( pos ) -- Particle emitter in this position
+
+for i = 1, 100 do -- Do 100 particles
+	local part = emitter:Add( "effects/spark", pos ) -- Create a new particle at pos
+		if ( part ) then
+			part:SetDieTime( 1 ) -- How long the particle should "live"
+
+			part:SetStartAlpha( 255 ) -- Starting alpha of the particle
+			part:SetEndAlpha( 0 ) -- Particle size at the end if its lifetime
+
+			part:SetStartSize( 5 ) -- Starting size
+			part:SetEndSize( 0 ) -- Size when removed
+
+			part:SetGravity( Vector( 0, 0, -250 ) ) -- Gravity of the particle
+			part:SetVelocity( VectorRand() * 50 ) -- Initial velocity of the particle
+		end
+	end
+
+	emitter:Finish()
+end
+
+
+if CLIENT then
+	surface.CreateFont( "FunnyAhFont", {
+		font = "CloseCaption_Bold",
+		extended = false,
+		size = 35,
+		weight = 500,
+		blursize = 0,
+		scanlines = 0,
+		antialias = true,
+		underline = false,
+		italic = false,
+		strikeout = false,
+		symbol = false,
+		rotary = false,
+		shadow = false,
+		additive = false,
+		outline = false,
+	})
+
+	function SWEP:DrawHUD()
+	local w = ScrW() / 2
+	local h = ScrH() / 2
+		draw.SimpleText( "Ammo: " .. self:Clip1(), "FunnyAhFont", w * 1.1, h * 1.05, Color(255, 255, 255), TEXT_ALIGN_RIGHT, TEXT_ALIGN_BOTTOM )
+		draw.SimpleText( "Charge: " .. self:Clip2(), "FunnyAhFont", w * 1.1025, h * 1.015, Color(255, 255, 255), TEXT_ALIGN_RIGHT, TEXT_ALIGN_BOTTOM )
+	end
+end
